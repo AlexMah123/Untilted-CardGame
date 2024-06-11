9906715d1747619dbf42ade8440e79438c847171
AlexMah123 <alexmahjy@gmail.com>
2024-06-11 23:21:24 +0800

    Relocated Files, Setup Basic UI Layout, Basic GameScene Loading
    * Cleaned up files
    * Prepared UI Layout
    * Basic MainMenu to GameScene, vice versa

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[Serializable]
+public enum Scene
+{
+    MENU,
+    GAMESCENE
+}
+
+
+public static class SceneLoader
+{
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene((int)scene);
+    }
+}
+
---
+++ Assets/Scripts/UserInterface/GameUIManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    [Header("Layout Groups")]
+    public GameObject GameplayGroup;
+    public GameObject SettingsGroup;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+#if UNITY_EDITOR
+        if (!GameplayGroup)
+        {
+            Debug.LogError("GameplayGroup is not assigned/does not exist");
+        }
+
+        if (!SettingsGroup)
+        {
+            Debug.LogError("SettingsGroup is not assigned/does not exist");
+        }
+#endif
+
+    }
+
+    public void ToggleSettingsGroup(bool isOn)
+    {
+        if (SettingsGroup.activeSelf != isOn)
+        {
+            SettingsGroup.SetActive(isOn);
+        }
+
+        if (!GameplayGroup.activeSelf)
+        {
+            GameplayGroup.SetActive(true);
+        }
+    }
+
+    public void ExitToMainMenu()
+    {
+        SceneLoader.Load(Scene.MENU);
+    }
+
+}
